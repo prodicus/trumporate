278040fbfc5ae30b0d825a1921544b871224afac
Rituraj Das <rituraj.das8@gmail.com>
2017-06-01 04:39:57 +0530

    breathing solid bg implemented across html, css and js

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+(function() {
+	
+	function color_gen(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	function bg_gen() {
+		document.body.style.background = "rgba("+color_gen(0,255)+","+color_gen(0,255)+","+color_gen(0,255)+",0.5)";	//standard
+		
+		document.body.style.transition = "all 2s ease";
+  		document.body.style.MozTransition = "all 2s ease";
+  		document.body.style.WebkitTransition = "all 2s ease";
+	}
+	
+	setInterval(bg_gen, 5000);
+	
+})();
