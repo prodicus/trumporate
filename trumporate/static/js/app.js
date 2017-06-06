ae09aee431421c8646c8077248fcba2e8b90507e
Rituraj Das <rituraj.das8@gmail.com>
2017-06-07 02:25:29 +0530

    commented spectrum cycling bg for static light-blue and started with JQuery-AJAX

--- trumporate/static/js/app.js
+++ trumporate/static/js/app.js
@@ -1,17 +1,30 @@
 (function() {
 	
-	function color_gen(min, max) {
+	//function to generate random numbers between min-max
+	/*function color_gen(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+	}*/
 	
-	function bg_gen() {
+	//function to change background to a random color
+	/*function bg_gen() {
 		document.body.style.background = "rgba("+color_gen(0,255)+","+color_gen(0,255)+","+color_gen(0,255)+",0.5)";	//standard
 		
 		document.body.style.transition = "all 2s ease";
   		document.body.style.MozTransition = "all 2s ease";
   		document.body.style.WebkitTransition = "all 2s ease";
-	}
+	}*/
 	
-	setInterval(bg_gen, 3000);
+	//setInterval(bg_gen, 3000);	//function call
+	
+	$.ajax({
+		type: "GET",
+		url: "",
+		success: function(data) {
+			
+		},
+		error: function() {
+			alert("Error loading data! Check connection.");
+		}
+	});
 	
 })();
