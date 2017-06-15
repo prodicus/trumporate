62d9247ca39ae5e4c2a66faadfc60b4b1d5a3067
Rituraj Das <rituraj.das8@gmail.com>
2017-06-16 04:07:02 +0530

    twitter-share button works entirely on twitter, needs custom update later

--- trumporate/static/js/app.js
+++ trumporate/static/js/app.js
@@ -1,15 +1,28 @@
 (function() {
-	
+	//run once
 	$.ajax({
 		type: 'GET',
 		url: 'https://crossorigin.me/http://35.160.0.244/api/v1/trump/rant',
-		success: function(data) {
-			//console.log(data.rant);
+		success: function(data) {					//console.log(data.rant);
 			$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
 		},
 		error: function() {
 			alert('Error loading data!');
 		}
+	});
+	
+	//refresh rants when #new_rant button is pressed
+	$('#new_rant').click(function() {
+		$.ajax({
+			type: 'GET',
+			url: 'https://crossorigin.me/http://35.160.0.244/api/v1/trump/rant',
+			success: function(data) {
+				$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
+			},
+			error: function() {
+				alert('Error loading data!');
+			}
+		});
 	});
 	
 })();
