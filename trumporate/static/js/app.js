450cee77c97f2159788621f853b9854d6fcae627
Rituraj Das <rituraj.das8@gmail.com>
2017-06-16 02:33:38 +0530

    making ajax requests via crossorigindotme proxy

--- trumporate/static/js/app.js
+++ trumporate/static/js/app.js
@@ -1,29 +1,14 @@
 (function() {
 	
-	//function to generate random numbers between min-max
-	/*function color_gen(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}*/
-	
-	//function to change background to a random color
-	/*function bg_gen() {
-		document.body.style.background = "rgba("+color_gen(0,255)+","+color_gen(0,255)+","+color_gen(0,255)+",0.5)";	//standard
-		
-		document.body.style.transition = "all 2s ease";
-  		document.body.style.MozTransition = "all 2s ease";
-  		document.body.style.WebkitTransition = "all 2s ease";
-	}*/
-	
-	//setInterval(bg_gen, 3000);	//function call
-	
 	$.ajax({
-		type: "GET",
-		url: "",
+		type: 'GET',
+		url: 'https://crossorigin.me/http://35.160.0.244/api/v1/trump/rant',
 		success: function(data) {
-			
+			//console.log(data.rant);
+			$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
 		},
 		error: function() {
-			alert("Error loading data! Check connection.");
+			alert('Error loading data!');
 		}
 	});
 	
