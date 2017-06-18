b036a63ddb4bbc83e8dbed69f76d21f4a8fe9909
Rituraj Das <rituraj.das8@gmail.com>
2017-06-19 02:27:04 +0530

    tweet w/quotes and opens new window

--- trumporate/api/v1/static/js/app.js
+++ trumporate/api/v1/static/js/app.js
@@ -5,7 +5,7 @@
 		url: '/api/v1/trump/rant/',
 		success: function(data) {					//console.log(data.rant);
 			$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
-			$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text='+data.rant+' ~Trumporate. Create your own dank memes at&url=https://trumporate.com" data-size="large"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>');
+			$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=\"'+data.rant+'\" ~Trumporate. Create your own dank memes at&url=https://trumporate.com$hashtags=meme%2Ctrumporate" data-size="large"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>');
 		},
 		error: function() {
 			alert('Error loading data!');
@@ -19,7 +19,7 @@
 			url: '/api/v1/trump/rant/',
 			success: function(data) {
 				$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
-				$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text='+data.rant+' ~Trumporate. Create your own dank memes at&url=https://trumporate.com" data-size="large"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>');
+				$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=\"'+data.rant+'\" ~Trumporate. Create your own dank memes at&url=https://trumporate.com" data-size="large"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>');
 			},
 			error: function() {
 				alert('Error loading data!');
@@ -29,20 +29,10 @@
 	
 	//open twitter-share in new tab
 	$('.twitter-share-button').click(function(event) {
-    	var width  = 575,
-        height = 400,
-        left   = ($(window).width()  - width)  / 2,
-        top    = ($(window).height() - height) / 2,
-        url    = this.href,
-        opts   = 'status=1' +
-                 ',width='  + width  +
-                 ',height=' + height +
-                 ',top='    + top    +
-                 ',left='   + left;
-    
-    window.open(url, 'twitter', opts);
- 
-    return false;
-  });
+    	e.preventDefault();
+    	var width = 500;
+    	var height = 300;
+    	window.open(this.href , 'twitter', 'width=' + width + ', height=' + height + ', top=' + ((window.innerHeight - height) / 2) + ', left=' + ((window.innerWidth - width) / 2));
+  	});
 	
 })();
