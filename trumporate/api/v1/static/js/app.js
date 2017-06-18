c44468e493a6bcbbf763c572960c367d3b43af46
Rituraj Das <rituraj.das8@gmail.com>
2017-06-19 03:58:06 +0530

    [app.js] proxy removed

--- trumporate/api/v1/static/js/app.js
+++ trumporate/api/v1/static/js/app.js
@@ -2,7 +2,7 @@
 	//run once
 	$.ajax({
 		type: 'GET',
-		url: 'http://crossorigin.me/http://35.160.0.244/api/v1/trump/rant/',
+		url: '/api/v1/trump/rant/',
 		success: function(data) {					//console.log(data.rant);
 			$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
 			$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=\'\''+data.rant+'\'\' ~&url=https://trumporate.com&hashtags=meme%2Ctrumporate" data-size="large" target="_blank"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>');
@@ -16,7 +16,7 @@
 	$('#new_rant').click(function() {
 		$.ajax({
 			type: 'GET',
-			url: 'http://crossorigin.me/http://35.160.0.244/api/v1/trump/rant/',
+			url: '/api/v1/trump/rant/',
 			success: function(data) {
 				$('.quotes').html('<h1><i class="fa fa-quote-left fa-lg" aria-hidden="true"></i> '+data.rant+' <i class="fa fa-quote-right fa-lg" aria-hidden="true"></i></h1>');
 				$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=\'\''+data.rant+'\'\' ~&url=https://trumporate.com&hashtags=meme%2Ctrumporate" data-size="large" target="_blank"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>');
